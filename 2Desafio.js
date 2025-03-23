8162cd14aefdc98c5e97141c696127d893078073
GustavoTeodoro10 <gustavo.teodoro@aluno.faculdadeimpacta.com.br>
2025-03-23 00:09:15 -0300

    Desafio 3 finalizado

--- 2Desafio.js
+++ 2Desafio.js
@@ -1,15 +1,29 @@
-let nome = prompt("Qual o seu nome?");
-let idade = prompt("Quantos anos você tem?");
-let linguagem = prompt("Qual linguagem de programação você está estudando?");
+function desafio2(){   
 
-alert(`Olá ${nome} , você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+    let nome = prompt("Qual o seu nome?");
+    while (nome == null || nome ==''){
+        alert('ERRO!!!')
+        nome = prompt("Qual o seu nome?")
+    }    
+    let idade = prompt("Quantos anos você tem?");
+    while (idade == null || idade ==''){
+        alert('ERRO!!!')
+        idade = prompt("Qual o seu nome?")
+    } 
+    let linguagem = prompt("Qual linguagem de programação você está estudando?");
+    while (linguagem == null || linguagem ==''){
+        alert('ERRO!!!')
+        linguagem = prompt("Qual o seu nome?")
 
-let resposta = prompt(`Você gosta de estudar ${linguagem} ? Responda com o número 1 para SIM ou 2 para Não`);
+    alert(`Olá ${nome} , você tem ${idade} anos e já está aprendendo ${linguagem}!`);
 
-if (resposta ==1){
-    alert("Muito bom! Continue estudando e você terá muito sucesso")
-    }   
-    
-if (resposta ==2){
-        alert("Ahh que pena... Já tentou aprender outras lingagens?")
+    let resposta = prompt(`Você gosta de estudar ${linguagem} ? Responda com o número 1 para SIM ou 2 para Não`);
+
+    if (resposta ==1){
+        alert("Muito bom! Continue estudando e você terá muito sucesso")
+        }   
+        
+    if (resposta ==2){
+            alert("Ahh que pena... Já tentou aprender outras lingagens?")
+    }
 }
