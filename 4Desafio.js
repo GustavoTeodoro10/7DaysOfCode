5fef1a5e6355de12b7d6f01b601f7bf19c7322c8
GustavoTeodoro10 <gustavo.teodoro@aluno.faculdadeimpacta.com.br>
2025-03-23 15:59:35 -0300

    Desafio 4 e trocamos as cores

---
+++ 4Desafio.js
@@ -0,0 +1,25 @@
+function desafio4(){
+
+    let numeroSecreto = parseInt(Math.random()*10+1);;
+    let chute = '';
+    let tentativa = 3;
+
+    console.log(numeroSecreto);
+
+    while (tentativa>0){
+        chute = prompt('Escolha um número entre 1 e 10')
+        if (chute == numeroSecreto){
+            alert(`Parabéns!\n Você acertou!\n O numero secreto é ${chute} !`)
+            return
+        }
+        else if(tentativa>0){
+            tentativa -=1;
+            if (tentativa == 1){
+            alert(`Você errou!\n Ainda tem ${tentativa} tentativa! `)}
+            else{
+                alert(`Você errou!\n Ainda tem ${tentativa} tentativas! `)
+            } 
+        }
+    }
+    alert('Tente outra vez!')
+}
