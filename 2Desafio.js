cecd6da631d24fb15ff70a763609acbd3b27edb8
LeonardoTeodoroSantos <teodoroleo@live.com>
2025-03-23 18:28:56 -0300

    Atualização do Desafio 2

--- 2Desafio.js
+++ 2Desafio.js
@@ -1,31 +1,31 @@
-function desafio2(){   
-
+function desafio2(){
     let nome = prompt("Qual o seu nome?");
     while (nome == null || nome ==''){
-        alert('ERRO!!!')
-        nome = prompt("Qual o seu nome?")
-    }    
+        alert('Erro, digite seu nome!');
+        nome = prompt("Qual o seu nome?");
+    }
+
     let idade = prompt("Quantos anos você tem?");
     while (idade == null || idade ==''){
-        alert('ERRO!!!')
-        idade = prompt("Qual o seu nome?")
-    } 
+        alert('Erro, digite sua idade!');
+        idade = prompt("Quantos anos você tem?");
+    }
+
     let linguagem = prompt("Qual linguagem de programação você está estudando?");
     while (linguagem == null || linguagem ==''){
-        alert('ERRO!!!')
-        linguagem = prompt("Qual o seu nome?")
-
+        alert('Erro, digite a linguagem que esta estudando!');
+        linguagem = prompt("Qual linguagem esta estudando?");
+    }
+    
     alert(`Olá ${nome} , você tem ${idade} anos e já está aprendendo ${linguagem}!`);
-
     let resposta = prompt(`Você gosta de estudar ${linguagem} ? Responda com o número 1 para SIM ou 2 para Não`);
 
-    if (resposta ==1){
-        alert("Muito bom! Continue estudando e você terá muito sucesso")
+    if (resposta == 1) {
+        alert("Muito bom! Continue estudando e você terá muito sucesso!")
         }   
         
-    if (resposta ==2){
+    if (resposta == 2) {
             alert("Ahh que pena... Já tentou aprender outras lingagens?")
     }
-    }
-    alert('FIM')
+    alert('FIM');
 }
